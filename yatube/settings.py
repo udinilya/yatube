1a00272ce8a5132ed7e21bc52a6b637adff11c42
udinilya <udin76@bk.ru>
2023-08-22 13:12:10 +0300

    changes requierements.txt

--- yatube/settings.py
+++ yatube/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+!o^_h77#%%d)es_))24kn@3!grr)eh!1gp)14baj6)%s#t2ku'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '158.160.69.16'
